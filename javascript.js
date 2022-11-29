3823c4efd2ffba0e0c3f784112a32ee034c6cb18
RedStoneCato <89419032+RedStoneCato@users.noreply.github.com>
2022-11-29 17:13:12 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ async function SuseyIPGrabber(){
     document.getElementById("amongus").innerHTML = "rocksolidsuseyfatcatsucker"
     const reg = await fetch("https://api.open-meteo.com/v1/forecast?latitude=45.4235&longitude=-75.6979&hourly=soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm,soil_moisture_0_1cm,soil_moisture_1_3cm,soil_moisture_3_9cm,soil_moisture_9_27cm,soil_moisture_27_81cm")
     const json = await reg.json()
-    const soilcrapandprap = json.hourly.soil_temperature_18cm
+    const soilcrapandprap = json.hourly
     document.getElementById("amongus").innerHTML = JSON.stringify(soilcrapandprap)
     //https://api.open-meteo.com/v1/forecast?latitude=45.4235&longitude=-75.6979&hourly=soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm,soil_moisture_0_1cm,soil_moisture_1_3cm,soil_moisture_3_9cm,soil_moisture_9_27cm,soil_moisture_27_81cm
 
